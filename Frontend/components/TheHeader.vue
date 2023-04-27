031bd714f63e8b9ba535e8cd45c7a2bc61757cd5
NikolinaZallemi <94118751+NikolinaZallemi@users.noreply.github.com>
2023-04-27 23:55:36 +0200

    header changes

--- Frontend/components/TheHeader.vue
+++ Frontend/components/TheHeader.vue
@@ -3,38 +3,72 @@
 -->
 <template>
     <header>
-        <h1>The Dog Shelter</h1>
+        <h1><img class="logo" src="~/assets/img/logo-witho-bck-slog.png"></h1>
         <nav>
-            <NuxtLink to = "/">HOME</NuxtLink>
-            <NuxtLink to = "/dogs">DOGS</NuxtLink>
-            <NuxtLink to = "/locations">LOCATIONS</NuxtLink>
-            <NuxtLink to = "/about">ABOUT US</NuxtLink>
-            <NuxtLink to = "/contact">CONTACT US</NuxtLink>
+            <span class="nav-link">About Us</span>
+            <span class="nav-link">Our Team</span>
+            <span class="nav-link">Investments</span>
+            <span class="nav-link">News</span>
+            <span class="nav-link">Get In Touch</span>
         </nav>
     </header>
 </template>
-
+  
 <style>
-    header
-    {
-        background-color: orange;
+header {
+    background-color: white;
+}
+
+header {
+    height: 3em;
+    padding: 0.5em 0.5%;
+    margin-bottom: 0.5em;
+    border-bottom: 0.2em solid;
+    border-image: linear-gradient(to right, rgb(91, 199, 204), rgb(9, 91, 119)) 1;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+nav {
+    display: flex;
+    gap: 3em;
+    font-size: 1.2rem;
+    text-decoration: rgb(25, 129, 129);
+    align-items: center;
+    flex-grow: 1;
+}
+
+.nav-link {
+    font-family: monospace;
+    font-weight: bold;
+    color: rgb(13, 84, 84);
+    cursor: pointer;
+    text-align: center;
+    padding: 0.5em;
+}
+
+.logo {
+    vertical-align: middle;
+    max-width: 16%;
+    max-height: 25%;
+    height: auto;
+    width: auto;
+    margin: auto;
+    padding-left: 5%;
+    ;
+}
+
+@media (max-width: 768px) {
+    .nav-link {
+        font-size: 1rem;
+        padding: 0.3em;
     }
 
-    header
-    {
-        padding: 0px 20px 0px 20px;
-        margin-bottom: 5px;
-        border-bottom: 5px solid brown;
-        
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
+    .logo {
+        max-width: 30%;
+        max-height: 20%;
     }
-
-    nav {
-        display: flex;
-        gap: 30px;
-        font-size: 17pt;
-        font-weight: bold;
-    }
+}
 </style>
--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -3,24 +3,24 @@
 -->
 <template>
     <main>
-        <img id = "main-img" src = "~/assets/img/home-image.jpg" />
+        <img id="main-img" src="~/assets/img/home-image.jpg" />
         <h2>
-            This is a description for the Dog's Hotel. The best place where dogs can rest.
+            This is a description for the Dog's Hotel.
         </h2>
     </main>
 </template>
 
 <style>
-    #main-img {
+#main-img {
     width: 80%;
     max-width: 1000px;
     height: auto;
-    }
+}
 
-    main {
-        width: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-    }
+main {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
 </style>
