3e5036efc5fb347023a5e17203dcfdfd41631900
NikolinaZallemi <94118751+NikolinaZallemi@users.noreply.github.com>
2023-05-05 23:39:28 +0200

    investments page started

--- Frontend/pages/locations/index.vue
+++ Frontend/pages/locations/index.vue
@@ -4,63 +4,103 @@
 -->
 <template>
     <main>
-        <h1>Locations</h1>
-        <div id="card-container">
-            <Card v-for = "location of locations" :title = "location.name" :subtitle = "location.city" :link = "'/locations/' + location.id" />
+        <div class="containeri">
+            <div class="image-containeri">
+                <div class="image-wrapperi">
+                    <img src="~/assets/img/logoinv.png" style="width: 100%;height: 100%;">
+                </div>
+
+            </div>
+            <div class="text-containeri">
+                <p>Investing in the most promising startups with the potential to change the
+                    world! </p>
+            </div>
         </div>
+        <!--<div id="card-container">
+            <Card v-for="location of locations" :title="location.name" :subtitle="location.city"
+                :link="'/locations/' + location.id" />
+        </div>-->
     </main>
 </template>
 
 <script>
-    /*
-        The defineNuxtComponent gets us access to the asyncData property.
-        This is the first function that is called by nuxt when the page is called.
-        We can use this to pre-load the data to make it available to the user.
-    */
-    export default defineNuxtComponent({
-        async asyncData() {
-            // useRuntimeConfig provide us with environment variables set up in the nuxtconfig file
-            const locations = await $fetch(useRuntimeConfig().public.serverURL + '/locations')
+import { pbkdf2 } from 'crypto';
 
-            return {
-                locations
-            }
+/*
+    The defineNuxtComponent gets us access to the asyncData property.
+    This is the first function that is called by nuxt when the page is called.
+    We can use this to pre-load the data to make it available to the user.
+*/
+/*export default defineNuxtComponent({
+    async asyncData() {
+        // useRuntimeConfig provide us with environment variables set up in the nuxtconfig file
+        const locations = await $fetch(useRuntimeConfig().public.serverURL + '/locations')
+
+        return {
+            locations
         }
-    })
+    }
+})*/
 </script>
-
 <style>
-    #card-container
-    {
-        display: flex;
-        flex-wrap: wrap;
-        flex-direction: row;
-        justify-content: center;
-        align-content: flex-start;
-        gap: 20px;
-    }
+.containeri {
+    background-color: #f5f5f5;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+}
 
-    main
-    {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-content: flex-start;
-        gap: 10px;
-    }
+.image-containeri {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: auto;
+    align-items: stretch;
+    max-width: 100%;
+}
 
-    #form-container {
-        width: 90%;
-        border-radius: 10px;
-        border: 2px solid brown;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-evenly;
-        align-content: flex-start;
-        gap: 20px;
+.image-wrapperi {
+    border-radius: 100%;
+    border: 3px solid #fff;
+    overflow: hidden;
+    padding-top: 100%;
+    position: relative;
+    width: 20rem;
+    height: 1.2rem;
+    margin: 0 auto;
+}
 
-        background-color: white;
-        padding: 20px;
-    }
+.image-wrapperi img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    height: 100%;
+    width: auto;
+    object-fit: cover;
+}
 
+.text-containeri {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+.text-containeri h1 {
+    text-align: center;
+    font-size: 1.2rem;
+    font-family: monospace;
+
+}
+
+.text-containeri p {
+    text-align: bottom;
+    font-size: 1rem;
+    font-family: monospace;
+
+}
 </style>
