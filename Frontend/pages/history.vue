61041e91596887477427bb8a18156d255f92aff8
Mirko Calcaterra <mirko-98@libero.it>
2023-05-13 10:09:13 +0200

    Funzia

--- Frontend/pages/history.vue
+++ Frontend/pages/history.vue
@@ -1,57 +1,24 @@
-
 <template>
-    <div class="carousel" @keydown="checkSlide($event)" tabindex="0">
-        <slot></slot>
-        <button @click.prevent="next" class="btn btn-next"><i></i></button>
-        <button @click.prevent="prev" class="btn btn-prev"><i></i></button>
-    </div>
+    <Carousel />
 </template>
+
 <script>
+import Carousel from '/pages/Carousel.vue'
+
 export default {
-    data() {
-        return {
-            index: 0,
-            slides: [],
-            slideDirection: '',
-        }
-    },
-    computed: {
-        slidesLength() {
-            return this.slides.length;
-        }
-    },
-    mounted() {
-        this.slides = this.$children;
-        this.slides.map((slide, index) => {
-            slide.index = index;
-        });
-    },
-    methods: {
-        next() {
-            this.index++;
-            if (this.index >= this.slidesLength) {
-                this.index = 0;
-            }
-            this.slideDirection = 'slide-right';
-        },
-        prev() {
-            this.index--;
-            if (this.index < 0) {
-                this.index = this.slidesLength - 1;
-            }
-            this.slideDirection = 'slide-left';
-        },
-        checkSlide(event) {
-            if (event.keyCode === 39) {
-                this.next();
-            } else if (event.keyCode === 37) {
-                this.prev();
-            } else {
-                return;
-            }
-        },
-    }
+  name: 'History',
+  components: {
+    Carousel
+  }
 }
 </script>
-<style></style>
-  
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #656565;
+  margin-top: 10px;
+}
+</style>
