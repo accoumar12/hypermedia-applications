c416540a5f2793faedb9e6c983aeec94651720aa
Mirko Calcaterra <mirko-98@libero.it>
2023-05-22 10:52:31 +0200

    Supervisor fixed

---
+++ Frontend/components/Supervisor.vue
@@ -0,0 +1,50 @@
+<!--
+    Card to display information in the list page.
+    This component could be achieved with the SmallCard component by using a <slot> to insert the div with the image. The solution used here allows for better control of the layout.
+    Since the information shown can be categorized into title and subtitle, it was preferred to use only one component for both dogs and location.
+
+    PROPS:
+    - title: main information to display
+    - subtitle: second information to display
+    - link: link to the page description
+-->
+<template>
+    <NuxtLink class="link" :to="link">
+        <div class="hover-content2">
+            <p><span class="subtitle">Supervisor: {{ name }}</span></p>
+        </div>
+    </NuxtLink>
+                    
+</template>
+
+<script setup>
+    const props = defineProps(['link', 'name']) 
+</script>
+
+<style>
+.link{
+    text-decoration: none;
+}
+
+.subtitle {
+    text-decoration: none;
+}
+
+.hover-content2 {
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    color: black;
+    text-decoration: none;
+}
+
+.hover-content p {
+    margin: 0;
+    color: black;
+    margin-top: 2%;
+    text-decoration: none;
+}
+
+</style>
--- Frontend/main.js
+++ Frontend/main.js
@@ -3,16 +3,7 @@ import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import App from './TheHeader.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue'
-library.add(
-  fas
-);
-Vue.defineComponent('font-awesome-icon', FontAwesomeIcon);
-Vue.use(BootstrapVue)
-Vue.use(BootstrapVueIcons)
-Vue.component('BIcon', BIcon)
-Vue.component('BIconHouseDoor', BIconHouseDoor)
 
 Vue.config.productionTip =false
 
