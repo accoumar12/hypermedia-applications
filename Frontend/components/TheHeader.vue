470de4ef854b53c40562a779671f794aaaf1e8e0
Mirko Calcaterra <mirko-98@libero.it>
2023-05-26 18:20:00 +0200

    Trial not well finished

--- Frontend/components/TheHeader.vue
+++ Frontend/components/TheHeader.vue
@@ -2,6 +2,7 @@
     Header of the page
 -->
 <template>
+    <div>
     <div v-if="!isMobile">
     <desktop> 
     <div class="header-container">
@@ -49,55 +50,33 @@
     </div>
     <div v-else>
     <mobile>
-        <div class="header-container">
+        <div class="navbar">
         <header>
-            <h4><router-link to="/"><img class="logo" src="~/assets/img/logo-witho-bck-slog.png"></router-link></h4>
-            <nav>
-                <span>
-                    <p><router-link to="/History" class="nav-link">About Us</router-link></p>
-                </span>
-                <router-link to="/people" class="nav-link">Our Team</router-link>
-                <div class="dropdown">
-                    <div class="dropdown-toggle">
-                        <router-link to="/companies" class="nav-link">Investments</router-link>
-                    </div>
-                    <div class="dropdown-menu">
-                        <p>
-                            <router-link to="/companies" class="nav-link">Areas</router-link>
-                        </p>
-                        <div class="sub-menu">
-                            <p><router-link to="/technology" class="nav-link">Technology</router-link></p>
-                            <p><router-link to="/healthcare" class="nav-link">Healthcare</router-link></p>
-                            <p><router-link to="/consumer" class="nav-link">Consumer Goods</router-link></p>
-                            <p><router-link to="/sustainability" class="nav-link">Sustainability</router-link></p>
-                        </div>
-                    </div>
-                </div>
-                <router-link to="/contact" class="nav-link">Get In Touch</router-link>
-                <div class="search">
-                    <input v-if="isSearchExpanded" type="text" :class="{ 'search-expanded': isSearchExpanded }"
-                        placeholder="Search" v-model="searchText" @keyup.enter="search" ref="searchInput" />
-                    <button @click="toggleSearch"><img src="~/assets/img/magnifying-glass.png" alt="search-icon"
-                            style="color: #2479a3;"></button>
-                    <!--
-                    <ul>
-                      <li v-for="result in searchResults" :key="result.text">
-                        <a :href="result.link">{{ result.text }}</a>
-                      </li>
-                    </ul>
-                -->
-                </div>
-            </nav>
+            <a class="navbar-item">
+                <router-link to="/"><img src="~/assets/img/logo-witho-bck-slog.png" alt="Vue.js Examples" width="40" height="40"></router-link>
+
+            </a>
+            <div id="navbarBurger" class="navbar-burger burger is-active" data-target="navMenuIndex">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            
         </header>
+        
     </div>
       </mobile>
   </div>
+  </div>
+
 </template>
 <script>
+
 const searchText = ref("");
 export default {
     data() {
         return {
+            isMobile: false,
             isSearchExpanded: false,
             searchText: '',
         };
@@ -132,15 +111,15 @@ export default {
             }
             return arrTot;
         },
-        isMobile() {
-           if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-             return true
-           } else {
-             return false
-           }
-         }
+        detectMobile() {
+          return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            navigator.userAgent
+          );
+        },
+        
     },
     mounted() {
+        this.isMobile = this.detectMobile();
         document.addEventListener("click", (event) => {
             if (!event.target.closest(".search")) {
                 this.isSearchExpanded = false;
@@ -334,4 +313,104 @@ nav {
         max-height: 20%;
     }
 }
+.navbar-brand, .navbar-tabs {
+  -webkit-box-align: stretch;
+  -ms-flex-align: stretch;
+  align-items: stretch;
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-negative: 0;
+  flex-shrink: 0;
+  min-height: 3.25rem;
+}
+
+.navbar-item {
+  font-size: 15px;
+  -webkit-box-flex: 0;
+  -ms-flex-positive: 0;
+  flex-grow: 0;
+  -ms-flex-negative: 0;
+  flex-shrink: 0;
+}
+
+.navbar-burger {
+  cursor: pointer;
+  display: block;
+  height: 60px;
+  position: relative;
+  width: 4.25rem;
+  right: 10px;
+  margin-left: auto;
+}
+
+
+
+.navbar-burger.is-active span:nth-child(1) {
+  -webkit-transform: translateY(5px) rotate(45deg);
+  transform: translateY(5px) rotate(45deg);
+}
+.navbar-burger span:nth-child(1) {
+  top: calc(50% - 6px);
+}
+.navbar-burger span {
+  background-color: currentColor;
+  display: block;
+  height: 1px;
+  left: calc(50% - 8px);
+  position: absolute;
+  -webkit-transform-origin: center;
+  transform-origin: center;
+  -webkit-transition-duration: 86ms;
+  transition-duration: 86ms;
+  -webkit-transition-property: background-color,opacity,-webkit-transform;
+  transition-property: background-color,opacity,-webkit-transform;
+  transition-property: background-color,opacity,transform;
+  transition-property: background-color,opacity,transform,-webkit-transform;
+  -webkit-transition-timing-function: ease-out;
+  transition-timing-function: ease-out;
+  width: 16px;
+}
+
+.navbar-menu.is-active {
+  display: block;
+}
+
+@media screen and (max-width: 1023px)
+.navbar-menu {
+  background-color: #fff;
+  -webkit-box-shadow: 0 8px 16px rgba(10,10,10,.1);
+  box-shadow: 0 8px 16px rgba(10,10,10,.1);
+  padding: .5rem 0;
+}
+.navbar-menu {
+  display: none;
+}
+
+
+.navbar-item.has-dropdown {
+
+    padding: 0;
+
+}
+.navbar-item {
+
+    font-size: 15px;
+    -webkit-box-flex: 0;
+    -ms-flex-positive: 0;
+    flex-grow: 0;
+    -ms-flex-negative: 0;
+    flex-shrink: 0;
+
+}
+.navbar-item, .navbar-link {
+
+    color: #4a4a4a;
+    display: block;
+    line-height: 1.5;
+    padding: .5rem .6rem;
+    position: relative;
+
+}
+
 </style>
