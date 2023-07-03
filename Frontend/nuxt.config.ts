c5a9f33bee979ec33075015680ab059a76c3f923
Mirko Calcaterra <mirko-98@libero.it>
2023-07-03 19:01:30 +0200

    I forgot to commit

--- Frontend/nuxt.config.ts
+++ Frontend/nuxt.config.ts
@@ -1,16 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    css: [
-        '~/assets/css/general.css'
-    ],
-    ssr: true,
-    runtimeConfig: {
-        public:{
-            serverURL: "http://localhost:3001" // URL of your local server
-        }
-    },
-    app: {
-        // Checking environment variable to decide whether the link 
-        baseURL: process.env.DEV? '' : '/VenTour'  // Name of your GitHub Repository
+  css: [
+    '~/assets/css/general.css'
+  ],
+  ssr: false, // Set ssr to false for Static Site Generation
+  target: 'static', // Specify the target as static
+  runtimeConfig: {
+    public: {
+      serverURL: "http://localhost:3001" // URL of your local server
     }
+  },
+  app: {
+    baseURL: process.env.DEV ? '' : '/VenTour' // Name of your GitHub Repository
+  }
 })
